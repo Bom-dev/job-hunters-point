10033a6b0883da50d0c76350e59396311e4a6d2a
Bom <11377ny@gmail.com>
2022-09-07 18:34:36 -0400

    Add header for axios call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom'
 import './App.css';
+import axios from 'axios';
 import Home from './components/Home';
 import Nav from './components/Nav';
 import Account from './components/Account';
@@ -10,6 +11,38 @@ import Interview from './components/Interview'
 
 
 function App() {
+
+  useEffect(() => {
+    getAllUser()
+  }, [])
+
+  // const getAllUser = () => {
+  //   axios.get(`https://job-hunters-point.herokuapp.com/users/`)
+  //   .then((r) => {
+  //       console.log(r.data)
+  //   })
+  //   .catch(e => {
+  //       console.log(e)
+  //   })
+  // }
+
+  const getAllUser = () => {
+    axios({
+      method: 'get',
+      url: `https://job-hunters-point.herokuapp.com/users/`,
+      headers: {
+        'Access-Control-Allow-Origin': 'https://localhost:3000',
+        'Access-Control-Allow-Headers': 'origin',
+        'Access-Control-Allow-Credentials': true,
+        crossorigin: true,
+      }
+    }).then(r => {
+      console.log(r)
+    }).catch(e => {
+      console.log(e)
+    })
+  }
+
   return (
     <div className="App">
       <Nav />
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,15 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
+
     return (
         <div>
             <header>
                 <nav>
                     <Link to='/'>
                         <h3>Home</h3>
-                    </Link>
-                    <Link to='/account'>
-                        <h3>Account</h3>
                     </Link>
                     <Link to='/applications'>
                         <h3>Application</h3>
@@ -20,6 +18,9 @@ const Nav = () => {
                     </Link>
                     <Link to='/interviews'>
                         <h3>Interview</h3>
+                    </Link>
+                    <Link to='/account'>
+                        <h3>Account</h3>
                     </Link>
                 </nav>
             </header>
